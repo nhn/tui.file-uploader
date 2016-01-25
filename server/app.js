a7e5d62ad973b1ac0eeb211c6eab66952f206ddd
minkyu-yi <minkyu.yi@nhnent.com>
2016-01-25 15:43:04 +0900

    feat: make app.js - node server example

---
+++ server/app.js
@@ -0,0 +1,11 @@
+/**
+ * File upload server example - NodeJS 5.1.0 stable
+ * NHN Entertainment FE development team
+ * Github:
+ *  nhnent/tui.component.file-uploader
+ *  https://github.com/nhnent/tui.component.file-uploader
+ **/
+
+var express = require('express');
+var router = express.Router();
+
