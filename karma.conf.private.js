db0719406735a7a2b3409acf3936d7ad71b62fc9
minkyu-yi <minkyu.yi@nhnent.com>
2016-02-04 11:58:44 +0900

    chore: add test browser=Edge

--- karma.conf.private.js
+++ karma.conf.private.js
@@ -80,6 +80,7 @@ module.exports = function(config) {
             'IE9',
             'IE10',
             'IE11',
+            'Edge',
             'Chrome-WebDriver',
             'Firefox-WebDriver'
         ],
@@ -114,6 +115,11 @@ module.exports = function(config) {
                 config: webdriverConfig,
                 browserName: 'internet explorer',
                 version: 11
+            },
+            'Edge': {
+                base: 'WebDriver',
+                config: webdriverConfig,
+                browserName: 'MicrosoftEdge'
             },
             'Chrome-WebDriver': {
                 base: 'WebDriver', 
